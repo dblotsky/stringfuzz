a2608e36a2f44596e6b37d0a2eceec8c89ee591f
Dmitry Blotsky <dmitry.blotsky@gmail.com>
2017-08-16 22:39:13 -0400

    Changing 'overlaps' generator to be correct.

--- stringfuzz/generators/overlaps.py
+++ stringfuzz/generators/overlaps.py
@@ -10,55 +10,64 @@ __all__ = [
 def random_string(size):
     return ''.join(random.choice(ALPHABET) for i in range(size))
 
-def make_verlaps(num_equalities, length_of_consts, length_of_vars):
+def chain_concats(length):
 
-    # create equalities of overlapping variables
-    variables   = []
-    expressions = []
-    model       = {}
-    for i in range(num_equalities):
+    # create left side
+    left_expr = smt_new_var()
+    left_vars = [left_expr]
 
-        # create new constants and variables
-        left               = smt_str_lit(random_string(length_of_consts))
-        right              = smt_str_lit(random_string(length_of_consts))
-        middle_a, middle_b = smt_new_var(), smt_new_var()
+    # base case
+    if length < 2:
+        return left_vars, left_expr
 
-        # create two concats
-        first_concat  = smt_concat(left, middle_a)
-        second_concat = smt_concat(middle_b, right)
+    # create right side
+    right_vars, right_expr = chain_concats(length - 1)
 
-        # create constraints
-        concat_constraint = smt_assert(smt_equal(
-            first_concat,
-            second_concat)
-        )
-        length_constraint_a = smt_assert(smt_equal(
-            smt_len(middle_a),
-            smt_int_lit(length_of_vars))
-        )
-        length_constraint_b = smt_assert(smt_equal(
-            smt_len(middle_b),
-            smt_int_lit(length_of_vars))
-        )
+    # build return value
+    all_vars = left_vars + right_vars
+    concat = smt_concat(left_expr, right_expr)
 
-        # add constraints
-        expressions.append(concat_constraint)
-        expressions.append(length_constraint_a)
-        expressions.append(length_constraint_b)
+    return all_vars, concat
 
-        # remember variables
-        variables.append(middle_a)
-        variables.append(middle_b)
+def make_verlaps(num_vars, length_of_vars):
 
-    # add sat-check
-    expressions.append(smt_sat())
+    # check args
+    num_middle_vars = num_vars - 2
+    if num_middle_vars < 1:
+        raise ValueError('the number of variables must be at least 3')
+
+    # create new variables
+    left                       = smt_new_var()
+    middle_vars, middle_concat = chain_concats(num_middle_vars)
+    right                      = smt_new_var()
+    variables                  = [left] + middle_vars + [right]
+
+    # create overlapping constraint
+    left_concat     = smt_concat(left, middle_concat)
+    right_concat    = smt_concat(middle_concat, right)
+    concat_equality = smt_assert(smt_equal(left_concat, right_concat))
+
+    # add constraint and sat-check
+    expressions = [
+        concat_equality,
+        smt_sat()
+    ]
 
     # create variable declarations
     declarations = []
     for v in variables:
         declarations.append(smt_declare_var(v))
 
-    return declarations + expressions
+    # create length constraints
+    length_constraints = []
+    for v in variables:
+        length_constraint = smt_assert(smt_equal(
+            smt_len(v),
+            smt_int_lit(length_of_vars))
+        )
+        length_constraints.append(length_constraint)
+
+    return declarations + length_constraints + expressions
 
 # public API
 def overlaps(*args, **kwargs):
