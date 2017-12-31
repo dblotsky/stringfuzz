9d8594807e0df2489c7af6016e7017a1dd1ca9c2
FedericoAureliano <fmorarocha@gmail.com>
2017-12-31 11:29:18 -0400

    adding simple ast rotation transformer

--- stringfuzz/transformers/__init__.py
+++ stringfuzz/transformers/__init__.py
@@ -1,2 +1,3 @@
 from stringfuzz.transformers.unprintable import *
 from stringfuzz.transformers.nop import *
+from stringfuzz.transformers.rotate import *
---
+++ stringfuzz/transformers/rotate.py
@@ -0,0 +1,79 @@
+import random
+import string
+
+from stringfuzz.ast import *
+from stringfuzz.parser import parse
+from stringfuzz.ast_walker import ASTWalker
+
+__all__ = [
+    'rotate',
+]
+
+class ASTTransformer(ASTWalker):
+    def __init__(self, ast):
+        super(ASTTransformer, self).__init__(ast)
+
+    @property
+    def ast(self):
+        return self._ASTWalker__ast
+
+    def exit_expression(self, expr):
+        if isinstance(expr, ConcatNode):
+            if isinstance(expr.body[0], ConcatNode):
+                # rotate clockwise ll, lr, r
+                temp = expr.body[1]
+                expr.body[1] = expr.body[0].body[0]
+                expr.body[0].body[0] = expr.body[0].body[1]
+                expr.body[0].body[1] = temp
+            if isinstance(expr.body[1], ConcatNode):
+                # rotate clockwise l, rl, rr
+                temp = expr.body[0]
+                expr.body[0] = expr.body[1].body[0]
+                expr.body[1].body[0] = expr.body[1].body[1]
+                expr.body[1].body[1] = temp   
+        elif isinstance(expr, ReConcatNode):
+            if isinstance(expr.body[0], ReConcatNode):
+                # rotate clockwise ll, lr, r
+                temp = expr.body[1]
+                expr.body[1] = expr.body[0].body[0]
+                expr.body[0].body[0] = expr.body[0].body[1]
+                expr.body[0].body[1] = temp
+            if isinstance(expr.body[1], ReConcatNode):
+                # rotate clockwise l, rl, rr
+                temp = expr.body[0]
+                expr.body[0] = expr.body[1].body[0]
+                expr.body[1].body[0] = expr.body[1].body[1]
+                expr.body[1].body[1] = temp
+        elif isinstance(expr, ReUnionNode):
+            if isinstance(expr.body[0], ReUnionNode):
+                # rotate clockwise ll, lr, r
+                temp = expr.body[1]
+                expr.body[1] = expr.body[0].body[0]
+                expr.body[0].body[0] = expr.body[0].body[1]
+                expr.body[0].body[1] = temp
+            if isinstance(expr.body[1], ReUnionNode):
+                # rotate clockwise l, rl, rr
+                temp = expr.body[0]
+                expr.body[0] = expr.body[1].body[0]
+                expr.body[1].body[0] = expr.body[1].body[1]
+                expr.body[1].body[1] = temp
+        elif isinstance(expr, InReNode):
+            if isinstance(expr.body[0], InReNode):
+                # rotate clockwise ll, lr, r
+                temp = expr.body[1]
+                expr.body[1] = expr.body[0].body[0]
+                expr.body[0].body[0] = expr.body[0].body[1]
+                expr.body[0].body[1] = temp
+            if isinstance(expr.body[1], InReNode):
+                # rotate clockwise l, rl, rr
+                temp = expr.body[0]
+                expr.body[0] = expr.body[1].body[0]
+                expr.body[1].body[0] = expr.body[1].body[1]
+                expr.body[1].body[1] = temp
+
+
+# public API
+def rotate(s, language):
+    expressions = parse(s, language)
+    transformer = ASTTransformer(expressions).walk()
+    return transformer.ast
