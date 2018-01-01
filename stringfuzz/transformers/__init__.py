41ca1e6742b967fb7ee16ced1f44592fa932536a
FedericoAureliano <fmorarocha@gmail.com>
2018-01-01 12:08:16 -0400

    adding extend and stub transformers

--- stringfuzz/transformers/__init__.py
+++ stringfuzz/transformers/__init__.py
@@ -2,3 +2,5 @@ from stringfuzz.transformers.unprintable import *
 from stringfuzz.transformers.nop import *
 from stringfuzz.transformers.rotate import *
 from stringfuzz.transformers.litfuzz import *
+from stringfuzz.transformers.extend import *
+from stringfuzz.transformers.stub import *
