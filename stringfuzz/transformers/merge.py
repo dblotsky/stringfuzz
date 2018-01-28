d174859d29302a6def0b24a89ca6a9c713087d99
FedericoAureliano <fmorarocha@gmail.com>
2018-01-28 12:41:14 -0500

    missed constant declarations and sorts

--- stringfuzz/transformers/merge.py
+++ stringfuzz/transformers/merge.py
@@ -2,9 +2,11 @@
 Combining two problems
 '''
 
-from stringfuzz.ast import ExpressionNode
+from stringfuzz.ast import ExpressionNode, SortNode
 from stringfuzz.ast_walker import ASTWalker
 from stringfuzz.smt import smt_string_logic, smt_sat
+
+DECLARATIONS = ["declare-fun", "declare-const"]
 
 __all__ = [
     'merge',
@@ -22,7 +24,10 @@ def assertion(expr):
     return isinstance(expr, ExpressionNode) and expr.symbol == "assert"
 
 def declaration(expr):
-    return isinstance(expr, ExpressionNode) and expr.symbol == "declare-fun"
+    return isinstance(expr, ExpressionNode) and expr.symbol in DECLARATIONS
+
+def sort(expr):
+    return isinstance(expr, SortNode)
 
 # public API
 def merge(first_ast, second_ast, rename_ids):
@@ -32,6 +37,7 @@ def merge(first_ast, second_ast, rename_ids):
 
     combined     = set(first_ast + second_ast)
     declarations = list(filter(declaration, combined))
+    sorts        = list(filter(sort, combined))
     assertions   = list(filter(assertion, combined))
-    transformed  = [smt_string_logic()] + declarations + assertions + [smt_sat()]
+    transformed  = [smt_string_logic()] + declarations + sorts + assertions + [smt_sat()]
     return transformed
