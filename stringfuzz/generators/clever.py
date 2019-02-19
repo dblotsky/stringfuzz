8e8e7724c0d07bc3418204e09fb0512514cb735c
Federico Mora <fmorarocha@gmail.com>
2019-02-18 19:29:09 -0500

    WIP: client-specific equivalence checking queries. Need to insert library calls

--- stringfuzz/generators/clever.py
+++ stringfuzz/generators/clever.py
@@ -3,15 +3,43 @@ import copy
 
 from stringfuzz.smt import *
 from stringfuzz.ast import *
-from stringfuzz.util import coin_toss
+from stringfuzz.util import coin_toss, random_string
 from stringfuzz.ast_walker import ASTWalker
 
 __all__ = [
     'clever',
 ]
 
-CURR_LIB_CALLS   = 0
-TARGET_LIB_CALLS = 0
+TREE_NONTERMINAL  = IfThenElseNode
+EXPR_NONTERMINALS = [
+    NotNode,
+    GtNode,
+    LtNode,
+    GteNode,
+    LteNode,
+    ContainsNode,
+    AtNode,
+    LengthNode,
+    IndexOf2Node,
+    PrefixOfNode,
+    SuffixOfNode,
+    StringReplaceNode,
+    SubstringNode, 
+    ConcatNode,
+    AndNode,
+    OrNode,
+    EqualNode
+]
+
+# global config
+_max_str_lit_length  = 0
+_max_int_lit         = 0
+_literal_probability = 0.0
+
+class LibInserter(ASTWalker):
+    def __init__(self, ast, lib_name, lib_sort, lib_args):
+        super().__init__(ast)
+        self.lib_call_depth = 0
 
 class LibRenamer(ASTWalker):
     def __init__(self, ast, old_name, new_name):
@@ -32,125 +60,164 @@ def call_func(name, signature, variables):
         return GenericExpressionNode(name, *[random.choice(variables[s]) for s in signature])
     return GenericExpressionNode(name, *variables)
 
-def generate_clients(client_name, client_sig, client_sort, lib_sig, old_lib_name, new_lib_name, max_client_depth, clever_depth):
-    # create variables
-    global CURR_LIB_CALLS
-    while CURR_LIB_CALLS != TARGET_LIB_CALLS:
-        CURR_LIB_CALLS = 0
-        variables = {s: [] for s in set(client_sig)}
-        decls = []
-        for s in client_sig:
-            v = smt_new_arg()
-            variables[s].append(v)
-            decls.append((v,s))
-        # create variable declarations
-        arg_declaration = smt_declare_args(decls)
+def get_all_returning_a(sort, nodes):
+    return list(filter(lambda node: node.returns(sort), nodes))
 
-        old_body = make_random_expression(max_client_depth, variables, clever_depth, [old_lib_name]+lib_sig)
-        
-        old_client = smt_define_func(client_name+"_old", arg_declaration, client_sort, old_body)
-        new_client = smt_define_func(client_name+"_new", arg_declaration, client_sort, LibRenamer([copy.deepcopy(old_body)], old_lib_name, new_lib_name).walk()[0])
+def make_random_literal(sort):
+    if sort == STRING_SORT:
+        return StringLitNode(random_string(_max_str_lit_length))
 
-    return [old_client, new_client]
+    if sort == INT_SORT:
+        return IntLitNode(random.randint(0, _max_int_lit))
 
-def generate_lib(name, signature, sort, depth):
-    # create variables
-    variables = {s: [] for s in set(signature)}
-    decls = []
-    for s in signature:
-        v = smt_new_arg()
-        variables[s].append(v)
-        decls.append((v,s))
-    # create variable declarations
-    arg_declaration = smt_declare_args(decls)
-
-    body = make_random_expression(depth, variables)
-    return smt_define_func(name, arg_declaration, sort, body)
-
-def make_random_expression(depth, variables, clever_depth=None, func=None):
-    global CURR_LIB_CALLS
-    # at depth 0, make a terminal
-    if depth < 1:
-        if coin_toss():
-            return random.choice(variables[BOOL_SORT])
+    if sort == BOOL_SORT:
         return BoolLitNode(coin_toss())
 
+    raise ValueError('unknown sort {}'.format(sort))
+
+def should_choose_literal():
+    global _literal_probability
+    return random.random() < _literal_probability
+
+def make_random_terminal(variables, sort):
+    # randomly choose between a variable or a literal
+    if should_choose_literal() or sort not in variables:
+        return make_random_literal(sort)
+
+    return random.choice(variables[sort])
+
+def make_random_expression(variables, sort, depth):
+    # at depth 0, make a terminal
+    if depth < 1:
+        return make_random_terminal(variables, sort)
+
     # randomly shrink the depth
-    shrunken_depth  = depth - 1 if coin_toss() else random.randint(0, depth - 1)
+    shrunken_depth = random.randint(0, depth - 1)
 
     # get random expression generator
-    if clever_depth and func and CURR_LIB_CALLS < TARGET_LIB_CALLS and coin_toss() and depth == clever_depth:
-        signature       = func[1:]
-        expression_node = lambda *x: call_func(func[0], signature, x)
-        CURR_LIB_CALLS += 1
-    else:
-        candidate_nodes = [AndNode, OrNode, NotNode, IfThenElseNode]
-        expression_node = random.choice(candidate_nodes)
-        signature       = expression_node.get_signature()
-    
-    num_args    = len(signature)
-    # Make everything bool
-    signature   = [BOOL_SORT for i in range(num_args)]
+    candidate_nodes = get_all_returning_a(sort, EXPR_NONTERMINALS)
+    expression_node = random.choice(candidate_nodes)
+    signature       = expression_node.get_signature()
+    num_args        = len(signature)
+
+    # if the expression takes any sort, pick one
+    if expression_node.accepts(ANY_SORT):
+        collapsed_sort = random.choice(DECLARABLE_SORTS)
+        signature      = [collapsed_sort for i in range(num_args)]
+
     # generate random arguments
-    random_args = [make_random_expression(shrunken_depth, variables, clever_depth, func) for arg_sort in signature]
+    random_args = [make_random_expression(variables, arg_sort, shrunken_depth) for arg_sort in signature]
+
     # build expression
-    expression  = expression_node(*random_args)
+    expression = expression_node(*random_args)
 
     return expression
 
-def make_clever(max_client_depth, num_client_vars, max_lib_depth, num_lib_vars, clever_depth, num_lib_calls, client_name="client", old_lib_name="old_lib", new_lib_name="new_lib"):
+def make_random_tree(variables, sort, tree_depth, expr_depth):
+    # at depth 0, make a terminal
+    if tree_depth < 1:
+        return make_random_expression(variables, sort, expr_depth)
+
+    # randomly shrink the depth
+    shrunken_depth = random.randint(0, tree_depth - 1)
+
+    # build expression
+    signature   = [sort, sort]
+    random_args = [make_random_tree(variables, arg_sort, shrunken_depth, expr_depth) for arg_sort in signature]
+    random_args = [make_random_expression(variables, BOOL_SORT, expr_depth)] + random_args
+    expression  = TREE_NONTERMINAL(*random_args)
+
+    return expression
+
+def make_clever(max_client_depth, num_client_vars, max_lib_depth, num_lib_vars, num_lib_calls, max_expr_depth, max_str_lit_length, max_int_lit, literal_probability, client_name="client", old_lib_name="old_lib", new_lib_name="new_lib"):
 
     # check args
     if max_client_depth < 1:
-        raise ValueError('the maximum depth must be at least 1')
+        raise ValueError('the maximum client depth must be at least 1')
+
+    if max_expr_depth < 1:
+        raise ValueError('the maximum expr depth must be at least 1')
 
     if num_lib_calls < 1:
         raise ValueError('the number of library calls must be at least 1')
 
     if max_lib_depth < 1:
-        raise ValueError('the maximum depth must be at least 1')
+        raise ValueError('the maximum library depth must be at least 1')
 
-    if num_lib_vars > num_client_vars:
-        raise ValueError('the library must have fewer aruments than the client')
+    global _max_str_lit_length
+    global _max_int_lit
+    global _literal_probability
 
-    global TARGET_LIB_CALLS
-    TARGET_LIB_CALLS = num_lib_calls
+    # set global config
+    _max_str_lit_length  = max_str_lit_length
+    _max_int_lit         = max_int_lit
+    _literal_probability = literal_probability
+
+    # create function definitions
+    lib_sort    = random.choice(DECLARABLE_SORTS)
+    lib_args    = [random.choice(DECLARABLE_SORTS) for s in range(num_lib_vars)]
+    client_sort = random.choice(DECLARABLE_SORTS)
+    client_args = [random.choice(DECLARABLE_SORTS) for s in range(num_client_vars)]
+
+    # create variables
+    variables = {s: [] for s in set(client_args)}
+    decls = []
+    for s in client_args:
+        v = smt_new_var()
+        variables[s].append(v)
+        decls.append(smt_declare_var(v, sort=s))
+
+    client_vars = {s: [] for s in set(client_args)}
+    client_decls = []
+    for s in client_args:
+        v = smt_new_arg()
+        client_vars[s].append(v)
+        client_decls.append((v,s))
+    # create variable declarations
+    client_decls = smt_declare_args(client_decls)
+
+    lib_vars = {s: [] for s in set(lib_args)}
+    lib_decls = []
+    for s in lib_args:
+        v = smt_new_arg()
+        lib_vars[s].append(v)
+        lib_decls.append((v,s))
+    # create variable declarations
+    lib_decls = smt_declare_args(lib_decls)
+
+    # Generate client and libs
+    old_lib_body = make_random_tree(lib_vars, lib_sort, max_lib_depth, max_expr_depth)
+    new_lib_body = make_random_tree(lib_vars, lib_sort, max_lib_depth, max_expr_depth)
+    
+    decls.append(smt_define_func(old_lib_name, lib_decls, lib_sort, old_lib_body))
+    decls.append(smt_define_func(new_lib_name, lib_decls, lib_sort, new_lib_body))
+
+    client_body = make_random_tree(client_vars, client_sort, max_client_depth, max_expr_depth)
+    inserter = LibInserter([client_body], old_lib_name, lib_args, lib_sort)
+    old_client_body = inserter.walk()[0]
+    lib_call_depth = inserter.lib_call_depth
+    new_client_body = LibRenamer([copy.deepcopy(old_client_body)], old_lib_name, new_lib_name).walk()[0]
+
+    decls.append(smt_define_func(client_name+"_old", client_decls, client_sort, old_client_body))
+    decls.append(smt_define_func(client_name+"_new", client_decls, client_sort, new_client_body))
 
     print(";BEGIN STRINGFUZZ STATS")
     print("; max_client_depth", max_client_depth)
     print("; num_client_vars", num_client_vars)
     print("; max_lib_depth", max_lib_depth)
     print("; num_lib_vars", num_lib_vars)
-    print("; lib_call_depth", clever_depth)
     print("; num_lib_calls", num_lib_calls)
+    print("; max_expr_depth", max_expr_depth)
+    print("; lib_call_depth", lib_call_depth)
     print(";END STRINGFUZZ STATS")
 
-    # create variables
-    variables = {s: [smt_new_var() for i in range(num_client_vars)] for s in [BOOL_SORT]}
-
-    # create variable declarations
-    variable_declarations = []
-    for s in [BOOL_SORT]:
-        new_variable_declarations = [smt_declare_var(v, sort=s) for v in variables[s]]
-        variable_declarations.extend(new_variable_declarations)
-
-    # create function definitions
-    lib_sig       = [BOOL_SORT for s in range(num_lib_vars)]
-    lib_sort      = BOOL_SORT
-    client_sort   = BOOL_SORT
-    client_sig    = [BOOL_SORT for s in range(num_client_vars)]
-    old_lib       = generate_lib(old_lib_name, lib_sig, lib_sort, max_lib_depth)
-    new_lib       = generate_lib(new_lib_name, lib_sig, lib_sort, max_lib_depth)
-    clients       = generate_clients(client_name, client_sig, client_sort, lib_sig, old_lib_name, new_lib_name, max_client_depth, clever_depth)
-    variable_declarations += [old_lib, new_lib] + clients
-
     # assert that the client calling the old library is not equal to the client calling the new library
-    asserts = [AssertNode(NotNode(EqualNode(call_func(client_name+"_old", client_sig, variables[BOOL_SORT]), call_func(client_name+"_new", client_sig, variables[BOOL_SORT]))))]
+    asserts = [AssertNode(NotNode(EqualNode(call_func(client_name+"_old", client_args, variables), call_func(client_name+"_new", client_args, variables))))]
 
     # add check-sat
     expressions = asserts + [CheckSatNode()]
 
-    return variable_declarations + expressions
+    return decls + expressions
 
 # public API
 def clever(*args, **kwargs):
